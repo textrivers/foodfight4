06a4cb639e6a7b907280ca3965434f58dbed64a5
textrivers <jon.p.woodward@gmail.com>
2022-11-16 08:40:09 -0500

    title screen improvements
    ...scrolling ice cream lights

--- Scenes/TitleScreen.gd
+++ Scenes/TitleScreen.gd
@@ -2,9 +2,17 @@ extends Node2D
 
 var next_scene = "res://Scenes/CharacterSelection.tscn"
 var game_start: bool = false
+var ice_cream_speed = 1
 
 func _ready():
 	pass # Replace with function body.
+
+func _physics_process(_delta):
+	for ice_cream in $IceCreamLights.get_children():
+		ice_cream.offset.x -= 1 * ice_cream_speed
+		if ice_cream.offset.x < -512:
+			ice_cream.offset.x = 2560
+			
 
 func _input(event):
 	if game_start == false:
